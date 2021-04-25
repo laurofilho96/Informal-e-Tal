6b8f469b72f89fe0ee19972a100573f8ca5ee9b8
laurofilho96 <laurofilho96@gmail.com>
2021-04-25 04:39:20 -0300

    Algumas coisas adicionadas
    Criei algumas classes para a implementação do BackEnd. Estou configurando a aplicação para utilizar o firebase, porém estou tendo problemas para importar os arquivos do gradle wrapper. Verei amanhã. Fazendo isso, criarei um método de autenticação de usuários com o google, assim o pessoal loga direto. Só passando pela tela de cadastro uma vez para alterar os dados.

---
+++ informaletal/lib/classes/ramo.dart
@@ -0,0 +1,14 @@
+class Ramo {
+
+  //nome da profissão
+  String _ramo;
+
+  String get ramo => _ramo;
+
+  set ramo(String ramo) {
+    _ramo = ramo;
+  }
+
+  //instanciando objeto
+  Ramo ramoDaEmpresa = new Ramo();
+}
